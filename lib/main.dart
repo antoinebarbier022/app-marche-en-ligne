82d6b5e04e7e0529b09fe3d6bf70a4fbd6144f46
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-15 23:50:16 +0100

    Construction de la page Home (en cours de test)

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(title: 'Online Market'),
+      home: HomePage(title: 'Online Market'),
     );
   }
 }
--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,27 +1,145 @@
 part of '_pages.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
+  HomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
+  final list = [
+    "Pomme",
+    "Banane",
+    "orange",
+    "citron",
+    "mangue",
+    "cerise",
+    "Pomme",
+    "Banane",
+    "orange",
+    "citron",
+    "mangue",
+    "cerise"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppBarCustom(title: 'Good Market',),
-      drawer: const SideBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Empty',
-            ),
-          ],
+        appBar: const AppBarCustom(
+          title: 'Good Market',
         ),
-      )
-    );
+        drawer: const SideBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Text("Search Bar"),
+                  Text("Store Departement"),
+                ],
+              ),
+              const Divider(
+                height: 20,
+                thickness: 1,
+                indent: 20,
+                endIndent: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                   Text("Shopping lists"),
+                  Icon(Icons.chevron_right)
+                ],),
+              ),
+              
+              SizedBox(
+                height: 150,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  // Let the ListView know how many items it needs to build.
+                  itemCount: list.length,
+
+                  // Provide a builder function. This is where the magic happens.
+                  // Convert each item into a widget based on the type of item it is.
+                  itemBuilder: (context, index) {
+                    final item = list[index];
+
+                    return Container(
+                        width: 100,
+                        padding: const EdgeInsets.all(10.0),
+                        //color: Colors.grey[200],
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AspectRatio(
+                              aspectRatio: 1 / 1,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                            Text(item),
+                            const Text("Prix : 5\$"),
+                            const Text("par kg"),
+                          ],
+                        ));
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const SizedBox(
+                      //width: 10,
+                    );
+                  },
+                ),
+              ),
+              const Divider(
+                height: 20,
+                thickness: 1,
+                indent: 20,
+                endIndent: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                   Text("Popular"),
+                  Icon(Icons.chevron_right)
+                ],),
+              ),
+              
+              GridView(
+                  shrinkWrap: true,
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    Container(color: Colors.red),
+                    Container(color: Colors.yellow),
+                    Container(color: Colors.blue),
+                    Container(color: Colors.green),
+                    Container(color: Colors.red),
+                    Container(color: Colors.yellow),
+                    Container(color: Colors.blue),
+                    Container(color: Colors.green),
+                    Container(color: Colors.red),
+                    Container(color: Colors.yellow),
+                    Container(color: Colors.blue),
+                    Container(color: Colors.green),
+                    Container(color: Colors.red),
+                    Container(color: Colors.yellow),
+                    Container(color: Colors.blue),
+                    Container(color: Colors.green)
+                  ],
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      childAspectRatio: 0.5,
+                      crossAxisSpacing: 10),
+                ),
+              
+            ],
+          ),
+        ));
   }
-
 }
-
