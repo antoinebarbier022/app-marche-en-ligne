43074c3de501239a0351ceef425ae55153cb3d7b
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-16 23:30:37 +0100

    Ajustement de la taille de la sidebar

--- lib/view/widgets/sidebar_widget.dart
+++ lib/view/widgets/sidebar_widget.dart
@@ -7,59 +7,62 @@ class SideBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Material(
-        color: Theme.of(context).primaryColor,
-        child: ListView(
-          padding: const EdgeInsets.only(top: 70.0),
-          children: ListTile.divideTiles(context: context, tiles: [
-            buildMenuItem(
-                text: "Store",
-                icon: Icons.store,
-                context: context,
-                link: HomePage(
-                  title: 'Home',
-                )),
-            buildMenuItem(
-                text: "Shopping Cart",
-                icon: Icons.shopping_cart,
-                context: context,
-                link: ShoppingCartPage(
-                  title: 'Shopping Cart',
-                )),
-            buildMenuItem(
-                text: "Shopping List",
-                icon: Icons.list,
-                context: context,
-                link: ShoppingListsPage(
-                  title: 'Shopping Lists',
-                )),
-            buildMenuItem(
-                text: "Recipes",
-                icon: Icons.menu_book,
-                context: context,
-                link: HomePage(title: "Home",)),
-            buildMenuItem(
-                text: "Order History",
-                icon: Icons.history,
-                context: context,
-                link: HomePage(title: "Home",)),
-            buildMenuItem(
-                text: "Login",
-                icon: Icons.login,
-                context: context,
-                link: HomePage(title: "Home",)),
-            buildMenuItem(
-                text: "Help",
-                icon: Icons.help_center,
-                context: context,
-                link: HomePage(title: "Home",)),
-            buildMenuItem(
-                text: "About",
-                icon: Icons.info,
-                context: context,
-                link: HomePage(title: "Home",)),
-          ]).toList(),
+    return SizedBox(
+      width:250,
+      child: Drawer(
+        child: Material(
+          color: Theme.of(context).primaryColor,
+          child: ListView(
+            padding: const EdgeInsets.only(top: 70.0),
+            children: ListTile.divideTiles(context: context, tiles: [
+              buildMenuItem(
+                  text: "Store",
+                  icon: Icons.store,
+                  context: context,
+                  link: HomePage(
+                    title: 'Home',
+                  )),
+              buildMenuItem(
+                  text: "Shopping Cart",
+                  icon: Icons.shopping_cart,
+                  context: context,
+                  link: ShoppingCartPage(
+                    title: 'Shopping Cart',
+                  )),
+              buildMenuItem(
+                  text: "Shopping List",
+                  icon: Icons.list,
+                  context: context,
+                  link: ShoppingListsPage(
+                    title: 'Shopping Lists',
+                  )),
+              buildMenuItem(
+                  text: "Recipes",
+                  icon: Icons.menu_book,
+                  context: context,
+                  link: HomePage(title: "Home",)),
+              buildMenuItem(
+                  text: "Order History",
+                  icon: Icons.history,
+                  context: context,
+                  link: HomePage(title: "Home",)),
+              buildMenuItem(
+                  text: "Login",
+                  icon: Icons.login,
+                  context: context,
+                  link: HomePage(title: "Home",)),
+              buildMenuItem(
+                  text: "Help",
+                  icon: Icons.help_center,
+                  context: context,
+                  link: HomePage(title: "Home",)),
+              buildMenuItem(
+                  text: "About",
+                  icon: Icons.info,
+                  context: context,
+                  link: HomePage(title: "Home",)),
+            ]).toList(),
+          ),
         ),
       ),
     );
