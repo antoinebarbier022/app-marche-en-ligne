0d7710e3e1b481d31813a6d82d64967bc0dca4a3
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-16 18:00:33 +0100

    Ajout de liens dans le menu sidebar

--- lib/view/widgets/sidebar_widget.dart
+++ lib/view/widgets/sidebar_widget.dart
@@ -12,31 +12,76 @@ class SideBar extends StatelessWidget {
         color: Theme.of(context).primaryColor,
         child: ListView(
           padding: const EdgeInsets.only(top: 70.0),
-          children: ListTile.divideTiles(
-              context: context,
-              tiles: [
-                buildMenuItem(text: "Store", icon: Icons.store),
-                buildMenuItem(text: "Shopping Cart", icon: Icons.shopping_cart),
-                buildMenuItem(text: "Shopping List", icon: Icons.list),
-                buildMenuItem(text: "Recipes", icon: Icons.menu_book),
-                buildMenuItem(text: "Order History", icon: Icons.history),
-                buildMenuItem(text: "Login", icon: Icons.login),
-                buildMenuItem(text: "Help", icon: Icons.help_center),
-                buildMenuItem(text: "About", icon: Icons.info),
-              ]).toList(),
+          children: ListTile.divideTiles(context: context, tiles: [
+            buildMenuItem(
+                text: "Store",
+                icon: Icons.store,
+                context: context,
+                link: HomePage(
+                  title: 'Home',
+                )),
+            buildMenuItem(
+                text: "Shopping Cart",
+                icon: Icons.shopping_cart,
+                context: context,
+                link: ShoppingCartPage(
+                  title: 'Shopping Cart',
+                )),
+            buildMenuItem(
+                text: "Shopping List",
+                icon: Icons.list,
+                context: context,
+                link: ShoppingCartPage(
+                  title: 'Shopping Cart',
+                )),
+            buildMenuItem(
+                text: "Recipes",
+                icon: Icons.menu_book,
+                context: context,
+                link: HomePage(title: "Home",)),
+            buildMenuItem(
+                text: "Order History",
+                icon: Icons.history,
+                context: context,
+                link: HomePage(title: "Home",)),
+            buildMenuItem(
+                text: "Login",
+                icon: Icons.login,
+                context: context,
+                link: HomePage(title: "Home",)),
+            buildMenuItem(
+                text: "Help",
+                icon: Icons.help_center,
+                context: context,
+                link: HomePage(title: "Home",)),
+            buildMenuItem(
+                text: "About",
+                icon: Icons.info,
+                context: context,
+                link: HomePage(title: "Home",)),
+          ]).toList(),
         ),
       ),
     );
   }
 }
 
-Widget buildMenuItem({required String text, required IconData icon}) {
+Widget buildMenuItem(
+    {required String text,
+    required IconData icon,
+    required BuildContext context,
+    required Widget link}) {
   return ListTile(
     leading: Icon(icon, color: Colors.white),
     title: Text(
       text,
       style: const TextStyle(color: Colors.white),
     ),
-    onTap: () => {},
+    onTap: () {
+      Navigator.push(
+        context,
+        CupertinoPageRoute(builder: (context) => link),
+      );
+    },
   );
 }
