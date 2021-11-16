8fa7cee9538de9fbbd956f48cba3ad2c2f3fdab1
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-16 21:55:00 +0100

    Ajout du DRAG sur les product item (pas encore mis en place le drop)

--- lib/view/widgets/product_item_widget.dart
+++ lib/view/widgets/product_item_widget.dart
@@ -14,36 +14,57 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(10),
-      child: Container(
-          width: 110,
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AspectRatio(
-                aspectRatio: 1 / 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: color,
-                    borderRadius: BorderRadius.circular(10),
+    return LongPressDraggable(
+      dragAnchorStrategy:
+          pointerDragAnchorStrategy, // Permet d'avoir l'élement positionné sur la souris (en haut à gauche)
+      feedback: FractionalTranslation(
+        translation: const Offset(
+            -0.5, -0.5), // Permet de centrer l'element sur la souris
+        child: Container(
+          width: 50,
+          child: AspectRatio(
+            aspectRatio: 1 / 1,
+            child: Container(
+              decoration: BoxDecoration(
+                color: color,
+                border: Border.all(color: Colors.black12, width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+            width: 110,
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 1 / 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: color,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
-              ),
-              Text(title),
-              Text("$price€/kg"),
-            ],
-          )),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => const ProductDetailPage(
-                    title: '',
-                  )),
-        );
-      },
+                Text(title),
+                Text("$price€/kg"),
+              ],
+            )),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const ProductDetailPage(
+                      title: '',
+                    )),
+          );
+        },
+      ),
     );
   }
 }
