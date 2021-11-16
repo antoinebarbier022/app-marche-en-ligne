7f2c9dcabb699a9c3f0035d8a5c036e5735e30d4
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-16 14:34:41 +0100

    Add tabs in product detail page

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Online Market',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/view/pages/product_detail_page.dart
+++ lib/view/pages/product_detail_page.dart
@@ -12,17 +12,73 @@ class ProductDetailPage extends StatelessWidget {
           title: 'Product détail',
         ),
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children:  [
-              ElevatedButton(
-                child: const Text("Back"),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+            child: Center(
+                child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 200,
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            const Text("Product X"),
+            const Text("Price €"),
+            const Text("per lb"),
+
+            DefaultTabController(
+            length: 4, // length of tabs
+            initialIndex: 0,
+            child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+              const TabBar(
+                labelColor: Colors.blue,
+                unselectedLabelColor: Colors.black,
+                tabs: [
+                  Tab(text: 'Tab 1'),
+                  Tab(text: 'Tab 2'),
+                  Tab(text: 'Tab 3'),
+                  Tab(text: 'Tab 4'),
+                ],
+              ),
+              Container(
+                height: 400, //height of TabBarView
+                decoration: const BoxDecoration(
+                  border: Border(top: BorderSide(color: Colors.grey, width: 0.5))
+                ),
+                child: TabBarView(children: <Widget>[
+                  Container(
+                    child: const Center(
+                      child: Text('Display Tab 1', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text('Display Tab 2', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text('Display Tab 3', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: const Center(
+                      child: Text('Display Tab 4', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ])
               )
-            ],
+            ])
           ),
-        ));
+            ],
+        ))));
   }
 }
