51191461cb69faf7eb1532af76d227c2e8feda20
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-18 15:28:45 +0100

    Ajout d'un fichier de configuration pour le thème de l'application (sera utilisé plus tard)

---
+++ lib/config/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppSizes{
+  static const int titleFontSize = 48;
+
+  // radius
+  static const int buttonRadius = 8;
+  static const int imageRadius = 8;
+}
+
+class AppColors{
+  static Color? primaryColor = Colors.green;
+
+  //couleurs pour les départements
+  static Color? red = Colors.red[100];
+  static Color? blue = Colors.blue[100];
+  static Color? green = Colors.green[100];
+  static Color? orange = Colors.orange[100];
+  static Color? yellow = Colors.yellow[100];
+  static Color? brown = Colors.brown[100];
+  static Color? purple = Colors.purple[100];
+  static Color? pink = Colors.pink[100];
+}
