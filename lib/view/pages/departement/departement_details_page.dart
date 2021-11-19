896472a04d48e1d99675db92b8ff6d89597b7c44
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-19 19:39:29 +0100

    Departement detail : on a placer la liste des badges dans un widget à part

--- lib/view/pages/departement/departement_details_page.dart
+++ lib/view/pages/departement/departement_details_page.dart
@@ -25,42 +25,9 @@ class DepartementDetailsPage extends StatelessWidget {
             padding: EdgeInsets.all(10.0),
             child: CupertinoSearchTextField(),
           ),
-          SizedBox(
-            height: 40,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              itemCount: departement.categories.length,
-              itemBuilder: (BuildContext context, int index) {
-                return InkWell(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                      margin: const EdgeInsets.all(5),
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColorLight,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                          child: Padding(
-                        padding: const EdgeInsets.only(left: 8, right: 8),
-                        child: Text(departement.categories[index].name),
-                      ))),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                          builder: (context) => DepartementCategoryPage(
-                                departement: departement,
-                                category: departement.categories[index],
-                              )),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-          const SizedBox(height:20),
+          // liste horizontale qui fait défiler les badges des catégories
+          CategoriesListBadge(departement: departement),
+          const SizedBox(height: 20),
           BlocBuilder<ProductBloc, ProductState>(
               builder: (context, productsState) {
             if (productsState is ProductsLoading) {
@@ -68,30 +35,77 @@ class DepartementDetailsPage extends StatelessWidget {
             } else if (productsState is ProductsLoaded) {
               return SizedBox(
                 child: ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: departement.categories.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return CollectionList(
-                        id: '',
-                        title: departement.categories[index].name,
-                        listProduct: productsState.products
-                            .where((i) =>
-                                (i.category ==
-                                departement.categories[index].name) && (i.departement ==
-                                departement.name))
-                            .toList(),
-                        link: DepartementCategoryPage(
-                          departement: departement,
-                          category: departement.categories[index],
-                        ));
-                  }
-                ),
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: departement.categories.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return CollectionList(
+                          id: '',
+                          title: departement.categories[index].name,
+                          listProduct: productsState.products
+                              .where((i) =>
+                                  (i.category ==
+                                      departement.categories[index].name) &&
+                                  (i.departement == departement.name))
+                              .toList(),
+                          link: DepartementCategoryPage(
+                            departement: departement,
+                            category: departement.categories[index],
+                          ));
+                    }),
               );
             } else {
               return const Center(child: Text("Is Empty."));
             }
           })
         ])));
+  }
+}
+
+class CategoriesListBadge extends StatelessWidget {
+  const CategoriesListBadge({
+    Key? key,
+    required this.departement,
+  }) : super(key: key);
+
+  final Departement departement;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: departement.categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+                margin: const EdgeInsets.all(5),
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColorLight,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                    child: Padding(
+                  padding: const EdgeInsets.only(left: 8, right: 8),
+                  child: Text(departement.categories[index].name),
+                ))),
+            onTap: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                    builder: (context) => DepartementCategoryPage(
+                          departement: departement,
+                          category: departement.categories[index],
+                        )),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
