dd4de7768f45705a70472ee13c3196144bedbdbd
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-21 12:40:41 +0100

    Ajout d'un badge de couleur rouge sur le panier (indique le nombre d'item dans le panier)

--- lib/view/widgets/_widgets.dart
+++ lib/view/widgets/_widgets.dart
@@ -2,6 +2,7 @@
 import 'package:app_market_online/buisness_logic/bloc/shop_bloc.dart';
 import 'package:app_market_online/data/models/_models.dart';
 import 'package:app_market_online/view/pages/_pages.dart';
+import 'package:badges/badges.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
--- lib/view/widgets/appbar_widget.dart
+++ lib/view/widgets/appbar_widget.dart
@@ -13,20 +13,61 @@ class AppBarCustom extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       centerTitle: true,
       title: Text(title),
-      automaticallyImplyLeading:true,
+      automaticallyImplyLeading: true,
       actions: [
-        IconButton(
-          onPressed: () {
-            Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ShoppingCartPage(
-                              title: '',
-                            )),
-                  );
+        BlocBuilder<ShopBloc, ShopState>(
+          builder: (context, state) {
+            if (state is ShopLoadSuccess) {
+              return Badge(
+                badgeContent: Text(
+                  "${state.cart.items.length}",
+                  style: const TextStyle(color: Colors.white, fontSize: 12),
+                ),
+                showBadge: state.cart.items.isNotEmpty,
+                badgeColor: Colors.red.shade400,
+                shape: BadgeShape.circle,
+                position: BadgePosition.topEnd(top: 2, end: 7),
+                toAnimate: false,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ShoppingCartPage(
+                                title: '',
+                              )),
+                    );
+                  },
+                  icon: const Icon(Icons.shopping_cart_outlined),
+                ),
+              );
+            } else {
+              return Badge(
+                badgeContent: const Text(
+                  "0",
+                  style: const TextStyle(color: Colors.white, fontSize: 12),
+                ),
+                showBadge: false,
+                badgeColor: Colors.red.shade400,
+                shape: BadgeShape.circle,
+                position: BadgePosition.topEnd(top: 2, end: 7),
+                toAnimate: false,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ShoppingCartPage(
+                                title: '',
+                              )),
+                    );
+                  },
+                  icon: const Icon(Icons.shopping_cart_outlined),
+                ),
+              );
+            }
           },
-          icon: const Icon(Icons.shopping_cart_outlined),
-        )
+        ),
       ],
     );
   }
