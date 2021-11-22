6bcfbb3026b712ddb6aa10fbdf207ebea0f1b9cd
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-22 17:39:35 +0100

    Bouton radio dans la boite de dialog shopping list qui selectionne une liste + bouton done

--- lib/view/widgets/dialogs/dialog_save_to_shopping_list_widget.dart
+++ lib/view/widgets/dialogs/dialog_save_to_shopping_list_widget.dart
@@ -1,9 +1,16 @@
 part of '../_widgets.dart';
 
-class ModalAddToShoppingList extends StatelessWidget {
+class ModalAddToShoppingList extends StatefulWidget {
   const ModalAddToShoppingList({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<ModalAddToShoppingList> createState() => _ModalAddToShoppingListState();
+}
+
+class _ModalAddToShoppingListState extends State<ModalAddToShoppingList> {
+  String? _shoppingListSelected = "";
 
   @override
   Widget build(BuildContext context) {
@@ -22,63 +29,66 @@ class ModalAddToShoppingList extends StatelessWidget {
           const Text('Add to shopping list'),
         ],
       ),
-      content: setupAlertDialoadContainer(),
+      content: BlocBuilder<ShoppingListBloc, ShoppingListState>(
+        builder: (context, state) {
+          if (state is ShoppingListLoadSuccess) {
+            return Container(
+              width: 300.0,
+              decoration: BoxDecoration(
+                //color: Colors.lightGreen.shade50,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: state.list.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return RadioListTile<String>(
+                      title: Text(state.list[index].name),
+                      value: state.list[index].name,
+                      groupValue: _shoppingListSelected,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _shoppingListSelected = state.list[index].name;
+                        });
+                      },
+                      selected: state.list[index].name == _shoppingListSelected,
+                      selectedTileColor: Colors.grey.withOpacity(0.1),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const SizedBox(
+                      height: 2,
+                    );
+                  }),
+            );
+          } else {
+            return const Text("Error");
+          }
+        },
+      ),
+      actionsAlignment: MainAxisAlignment.spaceBetween,
       actions: [
-        Container(
-          width: double.infinity,
-          child: ElevatedButton(
-            onPressed: () {
-              // si on veut fermer la fenetre de dialogue avant d'ouvrir l'autre
-              //Navigator.pop(context);
-              showDialog<String>(
-                context: context,
-                builder: (BuildContext context) =>
-                    const ModalAddNewShoppingList(),
-              );
-            },
-            child: const Text('New shopping list'),
-          ),
+        ElevatedButton(
+          onPressed: () {
+            // si on veut fermer la fenetre de dialogue avant d'ouvrir l'autre
+            //Navigator.pop(context);
+            showDialog<String>(
+              context: context,
+              builder: (BuildContext context) =>
+                  const ModalAddNewShoppingList(),
+            );
+          },
+          child: const Text('New shopping list'),
+        ),
+        ElevatedButton(
+          onPressed: _shoppingListSelected!.isEmpty ? null : () {
+            // si on veut fermer la fenetre de dialogue avant d'ouvrir l'autre
+            //Navigator.pop(context);
+            
+          },
+          child: const Text('Done'),
         ),
       ],
     );
   }
-}
-
-// shopping list à afficher dans la fenetre de dialog
-Widget setupAlertDialoadContainer() {
-  return BlocBuilder<ShoppingListBloc, ShoppingListState>(
-    builder: (context, state) {
-      if (state is ShoppingListLoadSuccess) {
-        return Container(
-          width: 300.0,
-          decoration: BoxDecoration(
-            color: Colors.lightGreen.shade50,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: ListView.separated(
-            shrinkWrap: true,
-            itemCount: state.list.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text(state.list[index].name),
-                trailing: const Radio(
-                  value: 1,
-                  groupValue: true,
-                  onChanged: null,
-                ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return Divider(
-                color: Colors.grey[300],
-                thickness: 1,
-              );
-            },
-          ),
-        );
-      }else{
-        return const Text("Error");
-      }
-    },
-  );
 }
