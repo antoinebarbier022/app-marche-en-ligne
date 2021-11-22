9956bafc8c87305d63697fa2c3c8fa126e352640
Antoine Barbier <emilebarbier022@gmail.com>
2021-11-22 19:22:37 +0100

    Nouveau design product detail

--- lib/view/pages/product/product_detail_page.dart
+++ lib/view/pages/product/product_detail_page.dart
@@ -21,8 +21,8 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
 
   void _decrementQuantity() {
     setState(() {
-      if (_quantity <= 0) {
-        _quantity = 0;
+      if (_quantity <= 1) {
+        _quantity = 1;
       } else {
         _quantity--;
       }
@@ -32,107 +32,159 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Product Detail"),
-          automaticallyImplyLeading: true,
-          actions: [
-            IconButton(
-                icon: const Icon(Icons.playlist_add),
-                onPressed: () => showDialog<String>(
-                      context: context,
-                      builder: (BuildContext context) =>
-                          ModalAddToShoppingList(product: widget.product,),
-                    ))
-          ],
+        appBar: const AppBarCustom(
+          title: "Product Detail",
         ),
         body: SingleChildScrollView(
-            child: Center(
-                child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 200,
-              child: AspectRatio(
-                aspectRatio: 1 / 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColorLight,
-                    borderRadius: BorderRadius.circular(10),
+            child: Container(
+          color: Theme.of(context).primaryColorLight,
+          child: Column(
+            children: [
+              Container(
+                width: 150,
+                child: AspectRatio(
+                  aspectRatio: 1 / 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColorLight,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(widget.product.name, style: const TextStyle(fontSize: 18)),
-            Text("${widget.product.price}€",
-                style: const TextStyle(fontWeight: FontWeight.bold)),
-            const Text("per lb"),
-            const SizedBox(
-              height: 40,
-            ),
-            DefaultTabController(
-                length: 3, // length of tabs
-                initialIndex: 0,
+
+              // Section content
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: const EdgeInsets.all(30.0),
                 child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      TabBar(
-                        labelColor: Theme.of(context).primaryColor,
-                        unselectedLabelColor: Colors.black,
-                        tabs: const [
-                          Tab(text: 'Details'),
-                          Tab(text: 'Nutrition'),
-                          Tab(text: 'Related'),
-                        ],
-                      ),
-                      Container(
-                          height: 300, //height of TabBarView
-                          decoration: const BoxDecoration(
-                              border: Border(
-                                  top: BorderSide(
-                                      color: Colors.grey, width: 0.5))),
-                          child: TabBarView(children: <Widget>[
-                            Container(
-                                padding: const EdgeInsets.all(20),
-                                child: RichText(
-                                  textAlign: TextAlign.justify,
-                                  text: const TextSpan(
-                                    text:
-                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel ligula non dolor volutpat dignissim. Ut vel tempor velit, quis vestibulum ante. Fusce eleifend tempus imperdiet. Mauris consequat interdum iaculis. Maecenas at hendrerit est, id pulvinar mauris. Nullam vel malesuada arcu. Aliquam sit amet nunc ac risus blandit elementum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam molestie massa lacus, ac lacinia odio ultricies id. Suspendisse vel augue nec libero condimentum commodo et a purus. Etiam massa odio, eleifend in imperdiet nec, posuere eget arcu. Nulla sollicitudin massa et nulla eleifend ullamcorper.",
-                                    style: TextStyle(color: Colors.black),
-                                  ),
-                                )),
-                            Container(
-                              child: const Center(
-                                child: Text('Display Tab 2',
-                                    style: TextStyle(
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.bold)),
-                              ),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(
+                              left: 10, right: 10, top: 5, bottom: 5),
+                          decoration: BoxDecoration(
+                            color:
+                                Theme.of(context).primaryColor.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Text(
+                              "€${widget.product.price.toStringAsPrecision(3)}",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Theme.of(context).primaryColorDark)),
+                        ),
+
+                        // Ajout du produit dans une liste
+                        InkWell(
+                          child: Container(
+                            padding: const EdgeInsets.only(
+                                left: 10, right: 10, top: 5, bottom: 5),
+                            decoration: BoxDecoration(
+                              color: Colors.red.withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(15),
                             ),
-                            Container(
-                              child: const Center(
-                                child: Text('Display Tab 3',
-                                    style: TextStyle(
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.bold)),
-                              ),
+                            child: const Icon(
+                              Icons.playlist_add,
+                              color: Colors.red,
                             ),
-                          ]))
-                    ])),
-          ],
-        ))),
+                          ),
+                          onTap: () => showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                ModalAddToShoppingList(
+                              product: widget.product,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    // Nom du produit
+                    Text(widget.product.name,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w900)),
+                    Text(widget.product.departement,
+                        style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400)),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    DefaultTabController(
+                        length: 3, // length of tabs
+                        initialIndex: 0,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              TabBar(
+                                labelColor: Theme.of(context).primaryColor,
+                                unselectedLabelColor: Colors.black,
+                                tabs: const [
+                                  Tab(text: 'Details'),
+                                  Tab(text: 'Nutrition'),
+                                  Tab(text: 'Related'),
+                                ],
+                              ),
+                              Container(
+                                  height: 200, //height of TabBarView
+                                  decoration: const BoxDecoration(
+                                      border: Border(
+                                          top: BorderSide(
+                                              color: Colors.grey, width: 0.5))),
+                                  child: TabBarView(children: <Widget>[
+                                    Container(
+                                        padding: const EdgeInsets.only(top: 20),
+                                        child: RichText(
+                                          textAlign: TextAlign.justify,
+                                          text: const TextSpan(
+                                            text:
+                                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel ligula non dolor volutpat dignissim. Ut vel tempor velit, quis vestibulum ante. Fusce eleifend tempus imperdiet. Mauris consequat interdum iaculis. Maecenas at hendrerit est, id pulvinar mauris. Nullam vel malesuada arcu. ",
+                                            style:
+                                                TextStyle(color: Colors.black),
+                                          ),
+                                        )),
+                                    Container(
+                                      child: const Center(
+                                        child: Text('Display Tab 2',
+                                            style: TextStyle(
+                                                fontSize: 22,
+                                                fontWeight: FontWeight.bold)),
+                                      ),
+                                    ),
+                                    Container(
+                                      child: const Center(
+                                        child: Text('Display Tab 3',
+                                            style: TextStyle(
+                                                fontSize: 22,
+                                                fontWeight: FontWeight.bold)),
+                                      ),
+                                    ),
+                                  ]))
+                            ])),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )),
         bottomSheet: BlocBuilder<ShopBloc, ShopState>(
           builder: (context, state) {
-if ((state is ShopLoadSuccess) ) {
-            bool productIsInCart = state.cart.items.any((item) => item.product.name == widget.product.name);
-            // si le produit n'est pas dans le panier
-            if (!productIsInCart ) {
+            if ((state is ShopLoadSuccess)) {
+              bool productIsInCart = state.cart.items
+                  .any((item) => item.product.name == widget.product.name);
+              // si le produit n'est pas dans le panier
+
               return Container(
                   width: double.infinity,
                   height: 60,
@@ -182,36 +234,15 @@ if ((state is ShopLoadSuccess) ) {
                           onPressed: () {
                             BlocProvider.of<ShopBloc>(context).add(
                                 ItemAdded(Item(widget.product, _quantity)));
-                            
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text(
+                                    'Vous avez ajouté [ ${_quantity.toInt()} x ${widget.product.name} ] dans votre panier.')));
                           },
                         ),
                       )
                     ],
                   ));
             } else {
-              
-              return Container(
-                  width: double.infinity,
-                  height: 60,
-                  color: Colors.grey[200],
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: SizedBox(
-                          width: 150,
-                          child: ElevatedButton(
-                            
-                            child: const Text("Already choosen ! See cart"),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ShoppingCartPage()),
-                              );
-                            },
-                          ),
-                        ),
-                  ));
-            }}else{
               return Container();
             }
           },
